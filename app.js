9841796ffc9bf3413b8bcfdd044ef45f50660b06
betanandes <robertafernandes46@gmail.com>
2024-07-25 18:08:16 -0300

    finalizado

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-alert("Boas vindas ao jogo do número secreto");
+alert("Boas vindas ao jogo do número secreto!");
 let numeroMaximo = 500;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
